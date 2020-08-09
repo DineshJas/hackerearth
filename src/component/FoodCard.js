cad02d21a624c6630e593fc72718c4f601ae70ec
Dinesh Kumar Kannan <dkannan@concertcare.com>
2020-08-09 08:58:41 +0530

    hover card added

--- src/component/FoodCard.js
+++ src/component/FoodCard.js
@@ -8,32 +8,64 @@ class Card extends Component {
     constructor(props) {
         super(props);
         this.state = { 
+            isHovering: false,
          }
     }
+
+    handleMouseHover() {
+        this.setState(this.toggleHoverState);
+    }
+
+    toggleHoverState(state) {
+    return {
+        isHovering: !state.isHovering,
+        };
+    }
+
     render() { 
         return ( 
             <>
                 <Link to={{ pathname: `/${this.props.id}`, state: {foodName: this.props.name}}} style={{textDecoration: "none"}}>
-                    <div className="card">
+                    <div className="card" onMouseEnter={() => {this.handleMouseHover()}} onMouseLeave={() => {this.handleMouseHover()}}>
+                        {!this.state.isHovering ?  
+                        <>
                         {
                         this.props.label ? <span className="recipe-label">{this.props.label}</span>
                         : ''
                         }
                         <img className="card-img-top" src={this.props.imageSrc} alt="food_image" />
                         <div className="card-body text-left" style={{color: 'black'}}>
-                        <div className="d-flex justify-content-between">
-                            <h3 className="card-title">{this.props.name}</h3>
-                            <img className="like-img-size" src={unlike} alt="like" />
+                            <div className="d-flex justify-content-between">
+                                <h3 className="card-title">{this.props.name}</h3>
+                                <img className="like-img-size" src={unlike} alt="like" />
+                            </div>
+                            <p style={{fontSize: "16px"}}>$<span className="mr-3"> {this.props.price}</span></p>
+                            <p className="card-text card-body-text">{this.props.description}</p>
                         </div>
-                        {/* <p style={{fontSize: "16px"}}><i className="far fa-clock"></i><span className="mr-3"> 25 mins </span></p> */}
-                        <p style={{fontSize: "16px"}}>$<span className="mr-3"> {this.props.price}</span></p>
-                        <p className="card-text card-body-text">{this.props.description}</p>
-                        </div>
+                        </> :
+                            <>
+                                <div className="middle-content">
+                                    <div style={customStyle}>VIEW MORE</div>
+                                    <br />
+                                    <div style={customStyle}>QUICK VIEW</div>
+                                </div>
+                            </>
+                        }
                     </div>
                 </Link>
             </>
          );
     }
+}
+
+const customStyle = {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    fontSize: '16px',
+    padding: '16px 32px',
+    margin: '20px',
+    borderRadius: '10px',
+    opacity: '0.6',
 }
  
 export default Card;
